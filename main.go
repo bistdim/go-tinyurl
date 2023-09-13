8d8b00dc2812f53e6be91601db3c5c31d3aa909e
dvbystriko <dvbystriko@mts.ru>
2023-09-13 14:02:39 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("."))
+	})
+
+	r.Mount("/v1/url", urlResource{}.Routes())
+
+	http.ListenAndServe(":3000", r)
+}
---
+++ url.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"math/rand"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+)
+
+type urlResource struct{}
+
+type hashResult struct {
+	TinyUrl string `json:"tinyUrl"`
+}
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func (rs urlResource) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Post("/", rs.Create) // POST /v1/url - create a new hash by url
+
+	return r
+}
+
+func (rs urlResource) Create(w http.ResponseWriter, r *http.Request) {
+	tinyUrl := generateRandomString(6)
+
+	render.JSON(w, r, &hashResult{TinyUrl: tinyUrl})
+
+}
